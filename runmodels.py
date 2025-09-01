77313b1c870682a1ff4ff925301f89084aa306d6
David Hersh <dhersh3094@gmail.com>
2025-09-01 10:02:23 +0200

    rot, jit, decim order

--- runmodels.py
+++ runmodels.py
@@ -230,20 +230,6 @@ def augmentation(config):
      min_subsample_distance = config.min_subsample_distance
 
      all_folders = train_folders + test_folders
-    
-     for folder in all_folders:
-        for las_file in os.listdir(folder):
-            if las_file.endswith('.laz'):
-
-                if 'intensity' in config.features:
-                    normalize_intensity(config, las_file=os.path.join(folder, las_file))
-
-                if 'normalize_xy' in augmentation_process:
-                    normalize_xy(las_file=os.path.join(folder, las_file))
-
-                if 'decimate' in augmentation_process:
-                    # print(f'Decimating by {config.decimation_percentage}%')
-                    decimate(config, las_file=os.path.join(folder, las_file))
                     
     # First poisson subsample, then rotate, jitter
      for folder in all_folders:
@@ -266,6 +252,20 @@ def augmentation(config):
             if las_file.endswith('.laz'):
                 if 'jitter' in augmentation_process and folder in train_folders:
                     jitter(config, las_file=os.path.join(folder, las_file))
+                    
+     for folder in all_folders:
+        for las_file in os.listdir(folder):
+            if las_file.endswith('.laz'):
+
+                if 'intensity' in config.features:
+                    normalize_intensity(config, las_file=os.path.join(folder, las_file))
+
+                if 'normalize_xy' in augmentation_process:
+                    normalize_xy(las_file=os.path.join(folder, las_file))
+
+                if 'decimate' in augmentation_process:
+                    # print(f'Decimating by {config.decimation_percentage}%')
+                    decimate(config, las_file=os.path.join(folder, las_file))
 
 
 
